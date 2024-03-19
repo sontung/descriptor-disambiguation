99c327e869e2fda77f4192a2100bf100bcffd313
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2024-03-19 22:10:30 +1000

    robot car-added superpoint

--- main_robotcar.py
+++ main_robotcar.py
@@ -83,10 +83,49 @@ def use_d2(train_ds_, test_ds_, using_global_descriptors):
     del trainer_
 
 
+def use_superpoint(train_ds_, test_ds_, using_global_descriptors):
+    conf, default_conf = dd_utils.hloc_conf_for_all_models()
+    local_desc_model = "superpoint"
+    model_dict = conf[local_desc_model]["model"]
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    Model = dynamic_load(extractors, model_dict["name"])
+    encoder = Model(model_dict).eval().to(device)
+    conf_ns = SimpleNamespace(**{**default_conf, **conf})
+    conf_ns.grayscale = conf[local_desc_model]["preprocessing"]["grayscale"]
+    conf_ns.resize_max = conf[local_desc_model]["preprocessing"]["resize_max"]
+
+    retrieval_model = "eigenplaces"
+    model_dict = conf[retrieval_model]["model"]
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    Model = dynamic_load(extractors, model_dict["name"])
+    model_dict.update(
+        {"variant": "EigenPlaces", "backbone": "ResNet101", "fc_output_dim": 2048}
+    )
+    encoder_global = Model(model_dict).eval().to(device)
+    conf_ns_retrieval = SimpleNamespace(**{**default_conf, **conf})
+    conf_ns_retrieval.resize_max = conf[retrieval_model]["preprocessing"]["resize_max"]
+    trainer_ = RobotCarTrainer(
+        train_ds_,
+        test_ds_,
+        512,
+        2048,
+        encoder,
+        encoder_global,
+        conf_ns,
+        conf_ns_retrieval,
+        using_global_descriptors,
+    )
+    trainer_.evaluate()
+    del trainer_
+
+
 if __name__ == "__main__":
     train_ds = RobotCarDataset()
     test_ds = RobotCarDataset(train=False, evaluate=True)
 
+    use_superpoint(train_ds, test_ds, False)
+    use_superpoint(train_ds, test_ds, True)
     use_d2(train_ds, test_ds, False)
     use_d2(train_ds, test_ds, True)
     use_r2d2(train_ds, test_ds, True)
