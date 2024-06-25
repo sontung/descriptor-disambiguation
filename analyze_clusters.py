ed90bb0ede4c0e5cd72aa40f462b860f9fcf7336
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2024-06-25 10:23:27 +1000

    ablation1

--- analyze_clusters.py
+++ analyze_clusters.py
@@ -3,7 +3,14 @@ import dd_utils
 
 
 def main():
-
+    xyz_global = "/home/n11373598/work/descriptor-disambiguation/output/robotcar/xyz-global.npy"
+    desc_global = "/home/n11373598/work/descriptor-disambiguation/output/robotcar/codebook-global.npy"
+    xyz_local = "/home/n11373598/work/descriptor-disambiguation/output/robotcar/xyz-local.npy"
+    desc_local = "/home/n11373598/work/descriptor-disambiguation/output/robotcar/codebook-local.npy"
+    xyz_global = np.load(xyz_global)
+    xyz_local = np.load(xyz_local)
+    desc_global = np.load(desc_global)
+    desc_local = np.load(desc_local)
     return
 
 
