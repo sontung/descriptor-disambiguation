1e6e392febc090e1a75219547a2111ebae0a47a5
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2024-07-10 13:14:27 +1000

    ablation-order2

--- clustering.py
+++ clustering.py
@@ -4,7 +4,7 @@ import sys
 import h5py
 import numpy as np
 import faiss
-import open3d as o3d
+# import open3d as o3d
 import pickle
 from pykdtree.kdtree import KDTree
 from sklearn.neighbors import BallTree
