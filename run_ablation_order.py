6a0d77147d77dd3a69ca7183e8d2abfb886f1b3d
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2024-07-10 23:08:55 +1000

    ablation-order7

--- run_ablation_order.py
+++ run_ablation_order.py
@@ -7,18 +7,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--dataset1",
+        "--dataset",
         type=str,
         default="datasets/aachen_v1.1",
         help="Path to the dataset, default: %(default)s",
     )
     parser.add_argument(
-        "--dataset2",
+        "--benchmark",
         type=str,
-        default="/work/qvpr/data/raw/2020VisualLocalization/RobotCar-Seasons",
+        default="aachen",
         help="Path to the dataset, default: %(default)s",
     )
     args = parser.parse_args()
-
-    main_aachen.run_ablation_order(args.dataset1)
-    main_robotcar.run_ablation_order(args.dataset2)
+    if args.benchmark == "aachen":
+        main_aachen.run_ablation_order(args.dataset)
+    elif args.benchmark == "robotcar":
+        main_robotcar.run_ablation_order(args.dataset)
+    else:
+        raise NotImplementedError
