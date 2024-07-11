0cb8bed5cd517310c8af763ef705aa5ab122eea6
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2024-07-11 19:36:59 +1000

    ablation-order17

--- trainer.py
+++ trainer.py
@@ -647,6 +647,7 @@ class RobotCarTrainer(BaseTrainer):
 
     def collect_descriptors(self, vis=False):
         features_h5 = self.load_local_features()
+        sfm_to_local_h5 = self.load_selected_local_features(features_h5)
         pid2mean_desc = np.zeros(
             (self.dataset.xyz_arr.shape[0], self.feature_dim),
             np.float64,
@@ -654,7 +655,7 @@ class RobotCarTrainer(BaseTrainer):
         pid2count = np.zeros(self.dataset.xyz_arr.shape[0], self.codebook_dtype)
 
         pid2mean_desc, pid2ind = self.collect_descriptors_loop(
-            features_h5, pid2mean_desc, pid2count, self.using_global_descriptors
+            features_h5, sfm_to_local_h5, pid2mean_desc, pid2count, self.using_global_descriptors
         )
         features_h5.close()
 
