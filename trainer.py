482cf21afdc4b86f748fc670632f6b1e26b34460
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2024-10-23 19:59:54 +1000

    heatmap6

--- trainer.py
+++ trainer.py
@@ -732,7 +732,7 @@ class RobotCarTrainer(BaseTrainer):
             f"{self.global_feature_dim}_"
             f"{self.lambda_val}_"
             f"{self.convert_to_db_desc}_"
-            f"{self.order}.h5",
+            f"{self.order}_{self.using_global_descriptors}.h5",
             "a",
             libver="latest",
         )
