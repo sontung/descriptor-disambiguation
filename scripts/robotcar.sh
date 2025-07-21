fc57b1fe571525f384b30808eed249c4cf69f7e0
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2025-07-21 12:20:22 +1000

    install

---
+++ scripts/robotcar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#PBS -N robotcar_dd
+#PBS -l select=1:ncpus=20:ngpus=1:mem=100GB:qlist=qvpr
+#PBS -l walltime=48:00:00
+#PBS -j oe
+
+set -e  # Exit on error
+
+# Note: gputype line is optional! Delete if any gpu is fine.
+
+# Activate your conda environment (should already be created and all packages installed)
+cd /home/n11373598/work/descriptor-disambiguation
+
+# Run the test command inside the pixi environment
+/home/n11373598/.pixi/bin/pixi run python main_robotcar.py --dataset /work/qvpr/data/raw/2020VisualLocalization/RobotCar-Seasons --local_desc d2net --local_desc_dim 512 --global_desc megaloc --global_desc_dim 8448 || {
+  echo "Python crashed!"
+  exit 1
+}
