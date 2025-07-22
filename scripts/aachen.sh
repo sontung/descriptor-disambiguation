34e9ef4ae9bfc0cc7138b1e09691f56aae6883b6
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2025-07-22 15:16:52 +1000

    clean

--- scripts/aachen.sh
+++ scripts/aachen.sh
@@ -12,7 +12,7 @@ set -e  # Exit on error
 cd /home/n11373598/work/descriptor-disambiguation
 
 # Run the test command inside the pixi environment
-/home/n11373598/.pixi/bin/pixi run python main_aachen.py --local_desc d2net --local_desc_dim 512 --global_desc megaloc --global_desc_dim 8448 || {
+/home/n11373598/.pixi/bin/pixi run python main_aachen.py --ablation 1 || {
   echo "Python crashed!"
   exit 1
 }
