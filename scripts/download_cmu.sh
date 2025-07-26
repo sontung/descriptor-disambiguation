d5d0fc282505405dc11e23e9dcec18e407e0c2a6
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2025-07-27 09:43:57 +1000

    pca

--- scripts/download_cmu.sh
+++ scripts/download_cmu.sh
@@ -6,8 +6,8 @@
 
 set -e  # Exit on error
 
-cd /home/n11373598/work/descriptor-disambiguation/datasets/
+#cd /home/n11373598/work/descriptor-disambiguation/datasets/
 
-wget -r -np -nH --cut-dirs=3 -R "index.html*" https://data.ciirc.cvut.cz/public/projects/2020VisualLocalization/Extended-CMU-Seasons/
-
+#wget -r -np -nH --cut-dirs=3 -R "index.html*" https://data.ciirc.cvut.cz/public/projects/2020VisualLocalization/Extended-CMU-Seasons/
+cd /home/n11373598/work/descriptor-disambiguation/datasets/Extended-CMU-Seasons
 for f in *.tar; do tar -xf "$f"; done
