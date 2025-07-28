0b7b305b473811737353c15bd33c5a6c5d1b1c5d
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2025-07-28 19:36:42 +1000

    final

--- scripts/aachen.sh
+++ scripts/aachen.sh
@@ -12,7 +12,7 @@ set -e  # Exit on error
 cd /home/n11373598/work/descriptor-disambiguation
 
 # Run the test command inside the pixi environment
-/home/n11373598/.pixi/bin/pixi run python main_aachen.py --convert 0 --ablation 1 --global_desc salad || {
+/home/n11373598/.pixi/bin/pixi run python main_aachen.py --convert 0 --global_desc salad || {
   echo "Python crashed!"
   exit 1
 }
